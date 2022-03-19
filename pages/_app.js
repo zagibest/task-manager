b9f72a23a8e8e8fd25bef36f3f70b533b319b28e
Zandan-Ochir Ganbold <zagig35@gmail.com>
2022-03-19 12:53:06 +0800

    fixded

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>NUMee</title>
+        <link rel="manifest" href="./site.webmanifest"></link>
+        <meta
+          name="viewport"
+          content="initial-scale=1, viewport-fit=cover, user-scalable=no"
+        ></meta>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
