9538fef2e401e6b58c279b98e915bb01468ef919
Zandan-Ochir Ganbold <zagig35@gmail.com>
2022-03-24 16:13:12 +0800

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -28,6 +28,21 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
       <Head>
         <title>NUMO - {language.title}</title>
         <link rel="manifest" href="./site.webmanifest"></link>
